70da2f79d3678c5bfb833b0eb6c9e0b12193ad73
Dennis The Menace <sdennisp25@hotmail.com>
2018-08-09 22:56:18 -0600

    added the win, lose, guesses, and user guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,56 @@
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+
+
+
+
+
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var guessesLeft = 10;
 var guessedLetters = [];
 var wins = 1;
 var losses = 1;
 
+//got the computer to guess random letters
+var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log(computerGuess);
 
 
 //most important part to fire the letters
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 	var userGuess = event.key;
 	console.log(event);
-	// guessedLetters.push(letters)
+	//adds the letter user presses to the guessedLetters
+	guessedLetters.push(event.key);
+	console.log(guessedLetters);
+	document.getElementById("lettersGuessed").innerHTML = guessedLetters;
 
-	//got the computer to guess random letters
-	var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-	console.log(computerGuess);
-	
+if (event.key === computerGuess) {
+	document.getElementById("yourWins").innerHTML = wins; wins++;
+	alert("YOU WIN");
+	resetgame();
 }
 
+else {
+	//takes points away from your guesses
+	guessesLeft--;
+	document.getElementById("guesses").innerHTML = guessesLeft;
+	console.log(guessesLeft);
+}
 
+if (guessesLeft ===0){
+	document.getElementById("yourLosses").innerHTML = losses; losses++;
+	alert("YOU LOSE");
+	resetgame();
 
+}
+function resetgame(){
+	var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+	guessesLeft = 10;
+	guessedLetters = []
 
+}
 
-
-
+}
 
 
 
