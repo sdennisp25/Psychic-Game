6991e9a5f435bfe99f69f2cff64d19ed206d216c
Dennis The Menace <sdennisp25@hotmail.com>
2018-08-09 19:31:31 -0600

    got the computer to choose a random letter

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Psychic-Game
+
+
+computer picks a letter
+user picks a letter
+	user letter compared to computer letter
+
+if user = - - compare, user win
+	else 
+		guesses - 
+	guess again
+
+0 - you lose
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,36 @@
-var Wins;
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var guessesLeft = 10;
+var guessedLetters = [];
+var wins = 1;
+var losses = 1;
 
 
-function myWin() {
-	for (var i = 0; i < 10; i++)
-		document.write(i);
-			
+
+//most important part to fire the letters
+document.onkeyup = function(event) {
+	var userGuess = event.key;
+	console.log(event);
+	// guessedLetters.push(letters)
+
+	//got the computer to guess random letters
+	var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+	console.log(computerGuess);
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+//guessletters.push(letter)
+// document.getElementById("wins").innerHTML = wins ++;
+//push.play()
+//wins.play()
+//resetgame()
+
